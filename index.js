b1e80bcbd57ec21b2c4c1932b580870fb1b2788e
Okenai <kassembayeva@gmail.com>
2021-12-22 15:23:56 -0600

    make it better

--- index.js
+++ index.js
@@ -4,15 +4,19 @@ const express = require("express");
 const cors = require("cors");
 const pool = require("./db/db");
 const path = require("path");
+
 //App Config
 const app = express();
 const port = process.env.PORT || 8080;
-app.use(express.static(path.join(__dirname, "front-end/build")))
+
+
+
 //Middleware
 app.use(express.json());
 app.use(cors());
+
 if(process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "front-end/build")))
+  app.use(express.static(path.join(__dirname, "client/build")))
 }
 //DB Config
 
@@ -310,7 +314,7 @@ app.delete("/api/tasks/:id", async (req, res) => {
 
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "front-end/build/index.html"))
+  res.sendFile(path.join(__dirname, "client/build/index.html"))
 })
 
 //Listener
