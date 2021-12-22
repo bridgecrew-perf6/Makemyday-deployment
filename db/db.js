cdeb6623ac2e5f4df677800f420d80bc79259e00
Okenai <kassembayeva@gmail.com>
2021-12-22 15:41:13 -0600

    make it better

--- db/db.js
+++ db/db.js
@@ -1,16 +1,18 @@
 const { Pool } = require("pg");
 
-const proConfig = {
-  connectionString: process.env.DATABASE_URL
-};
-
-const pool = new Pool(process.env.NODE_ENV === "production" ? proConfig : {
+const devConfig = {
   user: process.env.PGUSER,
   password: process.env.PGPASSWORD,
   host: process.env.PGHOST,
   port: process.env.PGPORT,
   database: process.env.PGDATABASE
-});
+};
+
+const proConfig = {
+  connectionString: process.env.URI
+};
+
+const pool = new Pool(process.env.NODE_ENV === "production" ? proConfig : devConfig);
 
 
 
